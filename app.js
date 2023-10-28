5388d3f1855f7f2f66f151a91c06a80bfa79537f
furozka <furozka05.github@gmail.com>
2023-10-29 00:15:58 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const fn = () => {
-    console.log('function');
-};
+const express = require("express");
+const app = express();
+const path = require("path");
+// console.log(path);
+app.use(express.static('public'));
+app.get("/", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+})
 
-fn();
+
+const port = 3000
+app.listen(3000, ()=>{ console.log("server is up")})
